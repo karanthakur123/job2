3ec2c937c57dfd6d4b9f9ba192cbe83e6e35e6aa
karanthakur123 <karanverma31@gmail.com>
2022-09-29 18:56:49 +0530

    f

---
+++ jobportalFinall/src/main/java/com/example/demo/JobportalFinallApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalFinallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalFinallApplication.class, args);
+	}
+
+}
